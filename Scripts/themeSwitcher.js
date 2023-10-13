df24317d1b7b96166060b780dfa6bd7740761c4d
Chris Anders <chris.anders@schwaebisch-hall.de>
2023-10-13 12:41:11 +0200

    Theme Switcher Icon hinzugefügt

--- Scripts/themeSwitcher.js
+++ Scripts/themeSwitcher.js
@@ -16,18 +16,34 @@ function setTheme() {
     var switchInput = document.querySelector("#theme_switch");
 
     localStorage.setItem("isDarkMode", switchInput.checked)
+
+    initThemeToggleIcon(switchInput.checked)
     refreshColors(getPalette(switchInput.checked));
 }
 
 function setThemeSwitch() {
     let isDarkMode = localStorage.getItem("isDarkMode") == "true";
     var switchInput = document.querySelector("#theme_switch");
+    
+    initThemeToggleIcon(isDarkMode);
 
     switchInput.checked = isDarkMode;
 };
 
+function initThemeToggleIcon(isDarkMode) {
+    var moonMask = document.querySelector("#moon-mask");
+
+    if (isDarkMode) {
+        moonMask.style.visibility = "visible";
+    }
+    else {
+        moonMask.style.visibility = "hidden";
+    }
+}
+
 function initTheme() {
     let isDarkMode = localStorage.getItem("isDarkMode") == "true";
+
     refreshColors(getPalette(isDarkMode));
 }
 
