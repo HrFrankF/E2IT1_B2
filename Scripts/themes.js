7e5fb07657368b8d19d5f48c41b26426ad12a7ac
Andolino13 <146931255+Andolino13@users.noreply.github.com>
2023-10-16 07:17:18 +0000

    Themes Änderung

--- Scripts/themes.js
+++ Scripts/themes.js
@@ -1,18 +1,18 @@
 let dark = {
-    font_color: "#fff",
-    primary_color: "#242A40",
-    secondary_color: "#24365E",
-    ternary_color: "#283555",
-    ternary_color_elevated: "#172A4A",
+    font_color: "#e6e6e6",                   //text color
+    primary_color: "#292929",                //background color body
+    secondary_color: "#b33434",              //header, footer background color
+    ternary_color: "#4f4e4e",                //navigation background color
+    ternary_color_elevated: "#bd4a4a",       
     images: {
         "#nav-logo-img": "logo_light.png"
     }
 };
 let light = {
     font_color: "#000",
-    primary_color: "#FFF",
-    secondary_color: "#E5E5E5",
-    ternary_color: "#F2F2F2",
+    primary_color: "#e8e8e8",
+    secondary_color: "#9e9e9e",
+    ternary_color: "#d1d1d1",           
     ternary_color_elevated: "#D9D9D9",
     images: {
         "#nav-logo-img": "logo_dark.png"
