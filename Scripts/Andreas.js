9082c62f68b428a5cc8392dc855e0f2a6913d4b1
Andolino13 <146931255+Andolino13@users.noreply.github.com>
2023-11-20 15:08:04 +0000

    Diashow Anpassung

--- Scripts/Andreas.js
+++ Scripts/Andreas.js
@@ -2,8 +2,9 @@ let imageIndex = 0;
 showSlides();
 
 function showSlides() {
+  const slides = document.getElementsByClassName("diaimages");
   let i;
-  const slides = document.getElementsByClassName("diashow-images");
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -11,6 +12,7 @@ function showSlides() {
   if (imageIndex > slides.length) {
     imageIndex = 1;
   }
+  
   slides[imageIndex - 1].style.display = "block";
   setTimeout(showSlides, 8000);
 }
