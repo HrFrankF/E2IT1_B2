9c671d83eda809fe70f040411a9567dce8af91b8
Andolino13 <146931255+Andolino13@users.noreply.github.com>
2023-11-20 15:17:00 +0000

    DOM Loader

--- Scripts/Andreas.js
+++ Scripts/Andreas.js
@@ -1,18 +1,21 @@
-let imageIndex = 0;
-showSlides();
-
-function showSlides() {
-  const slides = document.getElementsByClassName("diaimages");
-  let i;
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  imageIndex++;
-  if (imageIndex > slides.length) {
-    imageIndex = 1;
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    let imageIndex = 0;
+    showSlides();
   
-  slides[imageIndex - 1].style.display = "block";
-  setTimeout(showSlides, 8000);
-}
+    function showSlides() {
+      const slides = document.getElementsByClassName("diaimages");
+      let i;
+  
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      imageIndex++;
+      if (imageIndex > slides.length) {
+        imageIndex = 1;
+      }
+    
+      slides[imageIndex - 1].style.display = "block";
+      setTimeout(showSlides, 8000);
+    }
+  });
+  
