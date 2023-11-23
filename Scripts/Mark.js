acebbdf51b76ce4ff3b49e211656181b790a632b
Markwuerth <147056969+Markwuerth@users.noreply.github.com>
2023-11-23 11:15:14 +0000

    video

---
+++ Scripts/Mark.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("meinVideo");
+var btn = document.getElementById("meinBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
